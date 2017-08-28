a6c8c8384b75a00f8e22406b829617f1b78d4197
Matthew Potter <matthew_potter75@hotmail.com>
2017-08-28 21:02:34 +0100

    Updated PopulateGameweekFixture to avoid duplicate fixtures from PlayerHistory and Fixtures

--- FantasyPremierLeagueDB/PopuateGameweekFixtures.sql
+++ FantasyPremierLeagueDB/PopuateGameweekFixtures.sql
@@ -3,7 +3,7 @@
 MERGE INTO dbo.GameweekFixtures AS Target 
 USING 
 (
-	SELECT 0 AS Id, ph.gameweekId, t.id AS homeTeamId, ot.id AS awayTeamId, t.short_name AS homeTeam_Shortname, ot.short_name AS awayTeam_Shortname, ph.kickoff_time--, COUNT(DISTINCT ph.playerId) AS playerCount
+	SELECT ISNULL(f.id,0) AS id, ph.gameweekId, t.id AS homeTeamId, ot.id AS awayTeamId, t.short_name AS homeTeam_Shortname, ot.short_name AS awayTeam_Shortname, ph.kickoff_time--, COUNT(DISTINCT ph.playerId) AS playerCount
 	FROM dbo.PlayerHistory ph
 	INNER JOIN dbo.Teams ot
 	ON ph.opponent_teamId = ot.id
@@ -11,13 +11,17 @@ USING
 	ON ph.playerId = p.id
 	INNER JOIN dbo.Teams t
 	ON p.teamId = t.id
+	LEFT JOIN dbo.Fixtures f
+	ON ph.gameweekId = f.gameweekId
+	AND t.id = f.team_h
+	AND ot.id = f.team_a
 	WHERE ph.was_home = 1
-	GROUP BY ph.gameweekId, t.id, ot.id, t.short_name, ot.short_name, ph.kickoff_time
+	GROUP BY f.id, ph.gameweekId, t.id, ot.id, t.short_name, ot.short_name, ph.kickoff_time
 	HAVING COUNT(DISTINCT ph.playerId) > 2
 
 	UNION
 
-	SELECT f.Id, f.gameweekId, f.team_h, f.team_a, ht.short_name, at.short_name, f.kickoff_time
+	SELECT f.id, f.gameweekId, f.team_h, f.team_a, ht.short_name, at.short_name, f.kickoff_time
 	FROM dbo.Fixtures f
 	INNER JOIN dbo.Teams ht
 	ON f.team_h = ht.id
@@ -31,7 +35,6 @@ AND Target.homeTeamId = Source.homeTeamId
 AND Target.awayTeamId = Source.awayTeamId
 WHEN MATCHED THEN 
 UPDATE SET 
-Id = Source.Id,
 gameweekId = Source.gameweekId,
 homeTeamId = Source.homeTeamId,
 awayTeamId = Source.awayTeamId,
