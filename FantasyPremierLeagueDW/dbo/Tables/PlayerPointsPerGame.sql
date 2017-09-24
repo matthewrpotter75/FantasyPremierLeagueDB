abd5648069779e27f4983fe9ee4fb0573e4b733b
Matthew Potter <matthew_potter75@hotmail.com>
2017-09-24 05:48:04 +0100

    Corrections and enhancements to FutureFixtureAnalysis processing

---
+++ FantasyPremierLeagueDW/dbo/Tables/PlayerPointsPerGame.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE dbo.PlayerPointsPerGame
+(
+	PlayerKey INT NOT NULL,
+	PlayerPositionKey INT NOT NULL,
+	OpponentDifficulty TINYINT NOT NULL,
+	MinutesLimit INT NOT NULL,
+	Points SMALLINT NOT NULL,
+	Games TINYINT NOT NULL,
+	PlayerMinutes SMALLINT NOT NULL,
+	PPG DECIMAL(4,2) NOT NULL,
+	PPG5games DECIMAL(4,2),
+	PPG10games DECIMAL(4,2),
+	CONSTRAINT [PK_PlayerPointsPerGame] PRIMARY KEY CLUSTERED ([PlayerKey] ASC, [PlayerPositionKey] ASC, [OpponentDifficulty] ASC)
+);
---
+++ FantasyPremierLeagueDW/dbo/Tables/PlayerPointsPerGamePrevious5.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE dbo.PlayerPointsPerGamePrevious5
+(
+	PlayerKey INT NOT NULL,
+	PlayerPositionKey INT NOT NULL,
+	OpponentDifficulty TINYINT NOT NULL,
+	SeasonKey INT NOT NULL,
+	GameweekKey INT NOT NULL,
+	MinutesLimit INT NOT NULL,
+	Points SMALLINT NOT NULL,
+	Games TINYINT NOT NULL,
+	PlayerMinutes SMALLINT NOT NULL,
+	PPG DECIMAL(4,2) NOT NULL,
+	PPG5games DECIMAL(4,2),
+	PPG10games DECIMAL(4,2),
+	CONSTRAINT [PK_PlayerPointsPerGamePrevious5] PRIMARY KEY CLUSTERED ([SeasonKey] ASC, [GameweekKey] ASC, [PlayerKey] ASC, [PlayerPositionKey] ASC, [OpponentDifficulty] ASC)
+);
