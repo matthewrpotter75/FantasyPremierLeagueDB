5d29a5607b8c2a5a8dec8922396016b5af7afe0b
Matthew Potter <matthew_potter75@hotmail.com>
2019-02-17 02:17:45 +0000

    Adding extra columns to PlayerPrices Adding new table FactPlayerDailyAttributes to record daily changes to player attributes Remove PlayerDailyPricesKey from FactPlayerDailyPrices and make PlayerKey, DateKey the primary key

--- FantasyPremierLeagueDW/dbo/Tables/FactPlayerDailyPrices.sql
+++ FantasyPremierLeagueDW/dbo/Tables/FactPlayerDailyPrices.sql
@@ -1,17 +1,11 @@
 CREATE TABLE dbo.FactPlayerDailyPrices
 (
-	PlayerDailyPricesKey INT IDENTITY(1,1) NOT NULL,
 	PlayerKey INT NOT NULL,
 	TeamKey INT NOT NULL,
 	DateKey INT NOT NULL,
 	Cost TINYINT NOT NULL,
-	CONSTRAINT PK_FactPlayerDailyPrices PRIMARY KEY CLUSTERED (PlayerDailyPricesKey ASC),
+	CONSTRAINT PK_FactPlayerDailyPrices PRIMARY KEY CLUSTERED (PlayerKey ASC, DateKey ASC),
 	CONSTRAINT FK_FactPlayerDailyPrices_PlayerKey FOREIGN KEY(PlayerKey) REFERENCES dbo.DimPlayer ([PlayerKey]),
 	CONSTRAINT FK_FactPlayerDailyPrices_TeamKey FOREIGN KEY(TeamKey) REFERENCES dbo.DimTeam ([TeamKey]),
 	CONSTRAINT FK_FactPlayerDailyPrices_DateKey FOREIGN KEY(DateKey) REFERENCES dbo.DimDate ([DateKey])
 );
-GO
-
-CREATE UNIQUE NONCLUSTERED INDEX [UX_FactPlayerDailyPrices_PlayerKey_DateKey]
-    ON [dbo].[FactPlayerDailyPrices]([PlayerKey] ASC, [DateKey] ASC);
-GO
